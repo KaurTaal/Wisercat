b999608a1ea29e8f801bd37a9b0ea029f25b94f4
KaurTaal <taalkaur@gmail.com>
2024-05-23 22:37:32 +0300

    removed unused import

--- backend/src/main/java/com/wisercat/backend/BackendApplication.java
+++ backend/src/main/java/com/wisercat/backend/BackendApplication.java
@@ -3,7 +3,6 @@ package com.wisercat.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @SpringBootApplication
