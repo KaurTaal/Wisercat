f47c8b046fe5d8223ed889a0c8b060d000f3820e
KaurTaal <taalkaur@gmail.com>
2024-05-23 21:34:00 +0300

    Corrected endpoint url

--- backend/src/main/java/com/wisercat/backend/app/web/controllers/health/HealthController.java
+++ backend/src/main/java/com/wisercat/backend/app/web/controllers/health/HealthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping()
+@RequestMapping("api/health")
 public class HealthController {
 
     @GetMapping(path = "/check")
